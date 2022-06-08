a5df86413b1d0faef1e7c91eb43e5f046484f0df
Sheila Kirwan <sheilafleming81@gmail.com>
2022-06-08 22:33:07 +0100

    Exercise 2 completed using my own code.

--- src/components/exercises/02_embeddedVariables.js
+++ src/components/exercises/02_embeddedVariables.js
@@ -2,44 +2,37 @@ import React from "react";
 import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
 
 const Demo = () => {
-  const course = "Hdip Computing - Sheila Modules table based on solution.";
-  const modules = [
-    {
-      name: "DevOps",
-      noLectures: 9,
-      noPracticals: 3
-    },
-    {
-      name: "Enterprise Web Dev",
-      noLectures: 10,
-      noPracticals: 2
-    }
-  ];
+  const header = "HDIP Computer Science - Sheila Modules table 2 my own code";
+  const columnheader = ["Name", "No Lectures", "No Practicals"]
+  const modules = ["ICT 2", "Full Stack Dev", "Devops"];
+  const noLectures = ["1","2","3"]
+  const noPracticals = ["1","2","3"]
+
   return (
-    <div>
-      <h2>{course}</h2>
+    <>
+      <h1> {header} </h1>
       <table className="table table-bordered">
         <thead>
           <tr>
-            <th>Name</th>
-            <th>No lectures</th>
-            <th>No practicals</th>
+            <th>{columnheader[0]}</th>
+            <th>{columnheader[1]}</th>
+            <th>{columnheader[2]}</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td>{modules[0].name}</td>
-            <td>{modules[0].noLectures}</td>
-            <td>{modules[0].noPracticals}</td>
+            <td>{modules[0]}</td>
+            <td>{noLectures[2]}</td>
+            <td>{noPracticals[0]}</td>
           </tr>
           <tr>
-            <td>{modules[1].name}</td>
-            <td>{modules[1].noLectures}</td>
-            <td>{modules[1].noPracticals}</td>
+          <td>{modules[1]}</td>
+            <td>{noLectures[0]}</td>
+            <td>{noPracticals[1]}</td>
           </tr>
         </tbody>
       </table>
-    </div>
+    </>
   );
 };
 
